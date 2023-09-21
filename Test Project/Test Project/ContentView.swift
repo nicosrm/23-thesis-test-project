29a90cd7dff640b8b5edc4f7bb36933228b8100d
nicosrm <86198952+nicosrm@users.noreply.github.com>
2023-09-21 11:30:49 +0200

    Initial commit.

---
+++ Test Project/Test Project/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ButtonStyleTests.swift
+//  Test Project
+//
+//  Created by nicosrm
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Test Project/Test Project/Test_ProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  ButtonStyleTests.swift
+//  Test Project
+//
+//  Created by nicosrm
+//
+
+import SwiftUI
+
+@main
+struct Test_ProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
