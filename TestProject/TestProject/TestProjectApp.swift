24b0389f99179d1b2c97e55f4c29751b0aa4073d
Nico Schramm <86198952+nicosrm@users.noreply.github.com>
2023-10-15 16:56:31 +0200

    [RESET] Test project completely.

---
+++ TestProject/TestProject/TestProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestProjectApp.swift
+//  TestProject
+//
+//  Created by nicosrm
+//
+
+import SwiftUI
+
+@main
+struct TestProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
