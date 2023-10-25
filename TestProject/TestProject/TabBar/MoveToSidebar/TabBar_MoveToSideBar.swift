d4e0f43ccf1f65e12e7b32b1d674fc8c9fbfa560
nicosrm <86198952+nicosrm@users.noreply.github.com>
2023-10-25 09:59:53 +0200

    [FIX] Too many tabs non-conformities.

--- TestProject/TestProject/TabBar/MoveToSidebar/TabBar_MoveToSideBar.swift
+++ TestProject/TestProject/TabBar/MoveToSidebar/TabBar_MoveToSideBar.swift
@@ -9,6 +9,14 @@ import SwiftUI
 
 struct TabBar_MoveToSideBar: View {
 
+    @State private var selectionID: MenuItem.ID?
+
+    private let menuItems = [
+        MenuItem(name: "Fourth Tab", systemImage: "numbersign", tabText: "Tab 4"),
+        MenuItem(name: "Fifth Tab", systemImage: "numbersign", tabText: "Tab 5"),
+        MenuItem(name: "Sixth Tab", systemImage: "numbersign", tabText: "Tab 6")
+    ]
+
     var body: some View {
 
         TabView {
@@ -24,18 +32,22 @@ struct TabBar_MoveToSideBar: View {
                 .tabItem {
                     Label("Third Tab", systemImage: "numbersign")
                 }
-            Text("Tab 4")
-                .tabItem {
-                    Label("Fourth Tab", systemImage: "numbersign")
+
+            NavigationSplitView {
+                List(menuItems, selection: $selectionID) { item in
+                    Label(item.name, systemImage: item.systemImage)
                 }
-            Text("Tab 5")
-                .tabItem {
-                    Label("Fifth Tab", systemImage: "numbersign")
+                .navigationTitle("New Tab")
+            } detail: {
+                if let selectionID,
+                   let menuItem = menuItems.first(where: { $0.id == selectionID }) {
+                    Text(menuItem.tabText)
                 }
-            Text("Tab 6")
-                .tabItem {
-                    Label("Sixth Tab", systemImage: "numbersign")
-                }
+            }
+            .tabItem {
+                Label("Nested Tab", systemImage: "list.bullet")
+            }
+
             Text("Tab 7")
                 .tabItem {
                     Label("Seventh Tab", systemImage: "numbersign")
@@ -51,6 +63,20 @@ struct TabBar_MoveToSideBar: View {
         }
     }
 }
+
+// MARK: - MenuItem
+
+private extension TabBar_MoveToSideBar {
+
+    struct MenuItem: Identifiable, Hashable {
+        var id = UUID()
+        var name: String
+        var systemImage: String
+        var tabText: String
+    }
+}
+
+// MARK: - Preview
 
 #Preview {
     TabBar_MoveToSideBar()
